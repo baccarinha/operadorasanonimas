e229e07ba358ac5ed45190308bda2cfdfcff5be5
JÉSSICA THAIS BACCARO <jtbaccaro.jb@gmail.com>
2026-05-24 15:31:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+const ADMIN_UID = "jtbaccaro.jb@gmail.com";
+
 let usuarioAtual = null;
 
 const loginDiv = document.getElementById("login");
